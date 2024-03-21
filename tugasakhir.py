36f37d5b86541efea099a95e9ed3f466ec0e490e
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-03-21 14:26:47 +0700

    Update tugasakhir.py

--- tugasakhir.py
+++ tugasakhir.py
@@ -19,32 +19,20 @@ tab_titles = [
     "Klasifikasi SVM",
     "Uji Coba",]
 
-tabs = st.tabs(tab_titles)
-
-st.sidebar.write("""
-            # Pengertian Analisis Sentimen"""
-            )
-st.sidebar.write("""
-            Analisis sentimen adalah proses menganalisis teks digital untuk menentukan apakah nada emosional pesan tersebut positif, negatif, atau netral.
-            Alat analisis sentimen dapat memindai teks ini untuk secara otomatis menentukan sikap penulis terhadap suatu topik.
-            """
-            )
-st.sidebar.write("""
-            # Mengapa harus Analisis Sentimen?"""
-            )
-st.sidebar.write("""
-            1. Memberikan wawasan yang objektif
-            """
-            )
-st.sidebar.write("""
-            2. Membangun produk dan layanan yang lebih baik
-            """
-            )
-st.sidebar.write("""
-            3. Menganalisis dalam skala besar
-            """
-            )
-st.sidebar.write("""
-            4. Hasil waktu nyata
-            """
-            )
+with tabs[0]:
+    st.write("""
+    Air Terjun Dlundung di Kecamatan Trawas, Kabupaten Mojokerto menjadi salah satu destinasi wisata yang sayang untuk dilewatkan. Panorama alamnya memukau dan udaranya yang sejuk membuat liburan terasa singkat di sini.
+    """
+    )
+    st.write("""
+    Dlundung Waterfall berada di area Wana Wisata Dlundung. Tepatnya di Dusun/Desa Ketapanrame, Kecamatan Trawas. Wisata alam ini berjarak sekitar 38 kilometer dengan waktu tempuh sekitar 1 jam 10 menit dari Kota Mojokerto. Jika dari Kantor Kecamatan Trawas, Air Terjun Dlundung hanya sekitar 2,4 kilometer.
+    """
+    )
+    st.write("""
+    Air Terjun Dlundung mempunyai panorama alam yang eksotis. Karena lokasinya di antara hutan lereng Gunung Welirang yang lumayan lebat. Banyak pohon besar yang tumbuh di sekitarnya. Tak ayal, udara di wisata alam ini terasa sejuk. Sehingga, cocok untuk melepas penat bersama teman, keluarga atau kekasih tercinta.
+    """
+    )
+    st.write("""
+    Ketinggian Air Terjun Dlundung sekitar 14 meter. Air yang berjatuhan dari tebing tak terlalu deras. Sehingga, para pengunjung aman untuk bermain di bawahnya. Selain bermain air, para wisatawan juga bisa berswafoto di bawahnya. Dua spot foto yang tak kalah menarik di atas panggung sisi kanan air terjun dan di depan nama Dlundung Waterfall. Spot selfie juga bisa dijumpai di area parkir Air Terjun Dlundung. Yaitu dengan latar belakang nama Dlundung Waterfall dan lebatnya hutan di atasnya. Akses dari area parkir ke air terjun berupa tangga beton dan jalan paving di tengah rindangnya pepohonan.
+    """
+    )
