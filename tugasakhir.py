30e9bc5dd4aef3a489fa8102c510f51584b33415
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-03-21 11:22:31 +0700

    Update tugasakhir.py

--- tugasakhir.py
+++ tugasakhir.py
@@ -7,41 +7,45 @@ from collections import OrderedDict
 import seaborn as sns
 
 st.write("""
-# Prediksi Breast Cancer
-### KNN, Random Forest, Decicion Tree, Gaussian Naive Bayes
+# Analisis Sentimen Ulasan Air Terjun Dlundung
+### Metode K-NN
 """
 )
 
+img = Image.open('dlundung.jpg')
+st.image(img, use_column_width=False)
+
+tab_titles = [
+    "Homepage",
+    "Preposcessing",
+    "Wordcloud",]
+
+tabs = st.tabs(tab_titles)
+
 st.sidebar.write("""
-            # Penjelasan Untuk Pengisi Form"""
+            # Pengertian Analisis Sentimen"""
             )
 st.sidebar.write("""
-            ####  1. Usia: Diisi dengan angka usia calon pasien yang akan di prediksi
-            """)
+            Analisis sentimen adalah proses menganalisis teks digital untuk menentukan apakah nada emosional pesan tersebut positif, negatif, atau netral.
+            Alat analisis sentimen dapat memindai teks ini untuk secara otomatis menentukan sikap penulis terhadap suatu topik.
+            """
+            )
 st.sidebar.write("""
-            ####  2. BMI: Diisi dengan jumlah BMI yang ada dalam anda. Angka BMI normal berada pada kisaran 18,5-25.
-            """)
+            # Mengapa harus Analisis Sentimen?"""
+            )
 st.sidebar.write("""
-            ####  3. Glukosa: Diisi dengan jumlah glukosa dalam tubuh anda. Glukosa Normal berkisar < 100mg/dL, jika berpuasa 70-130 mg/dL, < 180 mg/dL (setelah makan), 100-140 mg/dL (sebelum tidur)
-            """)
+            1. Memberikan wawasan yang objektif
+            """
+            )
 st.sidebar.write("""
-            ####  4. Insulin: Diisi dengan jumlah insulin dalam tubuh anda. Insulin normal berkisar di bawah 100 mg/dL.
-            """)
+            2. Membangun produk dan layanan yang lebih baik
+            """
+            )
 st.sidebar.write("""
-            ####  5. HOMA: Diisi dengan jumlah HOMA dalam tubuh anda. homeostasis model aseessment (HOMA)
-            """)
+            3. Menganalisis dalam skala besar
+            """
+            )
 st.sidebar.write("""
-            ####  6. Leptin: Diisi dengan jumlah Leptin dalam tubuh anda. Leptin adalah suatu protein yang berasal dari 167 asam amino,merupakan hormon yang di produksi oleh jaringan adiposa. Biasa ditentukan dalam bentuk (ng/mL)
-            """)
-st.sidebar.write("""
-            ####  7. Adiponectin: Diisi dengan jumlah Adiponectin dalam tubuh anda.
-            """)           
-st.sidebar.write("""
-            ####  8. Resistin: Diisi dengan jumlah resistin dalam tubuh anda. Biasa ditentukan dalam bentuk (ng/mL)
-            """)
-st.sidebar.write("""
-            ####  9. MCP: Diisi dengan jumlah MCP dalam tubuh anda. MCP (Monocyte Chemoattracttant Protein-1). Biasa ditentukan dalam bentuk (pg/dL)
-            """)
-st.sidebar.write("""
-            ####  10. Setelah semuanya terisi silahkan klik prediksi untuk mengetahui hasil dari prediksi tersebut
-            """)
+            4. Hasil waktu nyata
+            """
+            )
