10343b07cbf842939f339db86694802c44106d0b
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-03-21 14:39:29 +0700

    Update tugasakhir.py

--- tugasakhir.py
+++ tugasakhir.py
@@ -9,20 +9,24 @@ import seaborn as sns
 import streamlit as st
 
 # Judul navbar
-st.sidebar.title('Navigation')
+st.sidebar.title('Main Menu')
 
 # Pilihan menu dalam bentuk dropdown
-menu_selection = st.sidebar.selectbox('Go to', ['Home', 'About', 'Contact'])
+menu_selection = st.sidebar.selectbox('Klik Tombol Di bawah ini', ['Home', 'Pre-Pocesssing Data', 'Klasifikasi SVM','Uji Coba'])
 
 # Membuat konten berdasarkan pilihan menu
 if menu_selection == 'Home':
-    st.title('Welcome to the Home Page')
+    st.title('Selamat Data di Halaman Home Page')
     st.write('This is the homepage content.')
 
-elif menu_selection == 'About':
-    st.title('About Us')
+elif menu_selection == 'Pre-Pocesssing Data':
+    st.title('Halaman Pre-pocessing Data')
     st.write('This is the about us page.')
 
-elif menu_selection == 'Contact':
-    st.title('Contact Us')
+elif menu_selection == ''Klasifikasi SVM'':
+    st.title('Halaman Klasifikasi SVM')
+    st.write('This is the contact us page.')
+
+elif menu_selection == ''Uji Coba'':
+    st.title('Halaman Uji Coba')
     st.write('This is the contact us page.')
