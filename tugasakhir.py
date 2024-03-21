b3c3b267611ee7ac56c60909165126443c680e0e
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-03-21 13:48:01 +0700

    Update tugasakhir.py

--- tugasakhir.py
+++ tugasakhir.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
