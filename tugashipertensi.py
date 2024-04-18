b3904f0acf0266f95599f7713fa12654c3a35976
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-04-18 10:22:52 +0700

    Update tugashipertensi.py

--- tugashipertensi.py
+++ tugashipertensi.py
@@ -80,7 +80,7 @@ def main():
         upload_file = st.sidebar.file_uploader("Masukkan file csv disini", key=1)
     
     if selected == 'Home':
-        st.markdown('<h1 style="text-align: center;"> Selamat Data di Website Klasifikasi Hipertensi </h1>', unsafe_allow_html=True)
+        st.markdown('<h1 style="text-align: center;"> Selamat Datang di Website Klasifikasi Hipertensi </h1>', unsafe_allow_html=True)
         st.markdown('<h3 style="text-align: left;"> Hipertensi </h1>', unsafe_allow_html=True)
         st.write('Hipertensi adalah kondisi yang terjadi ketika tekanan darah naik di atas kisaran normal, biasanya masyarakat menyebutnya darah tinggi. Penyakit hipertensi berkaitan dengan kenaikan tekanan darah di sistolik maupun diastolik. Faktor faktor yang berperan untuk penyakit ini adalah perubahan gaya hidup, asupan makanan dengan kadar lemak tinggi, dan kurangnya aktivitas fisik seperti olahraga')
         st.write('Faktor Faktor Resiko Hipertensi')
