294593aef9e5f1cebb57bb8cb456413ed1c1b84e
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-04-26 10:33:43 +0700

    Update tugashipertensi.py

--- tugashipertensi.py
+++ tugashipertensi.py
@@ -59,6 +59,11 @@ def normalize_data(data):
     data = data.drop_duplicates()
     return data
 
+def load_svm_model():
+    # Di sini Anda harus mengganti 'nama_model.pkl' dengan nama file yang sesuai dengan model SVM Anda
+    model = joblib.load('modelbagging.pkl')
+    return model
+
 def main():
     with st.sidebar:
         selected = option_menu(
@@ -215,11 +220,6 @@ def main():
     elif selected == 'Uji Coba':
         st.title("Uji Coba")
         st.write("Masukkan nilai untuk pengujian:")
-
-        def load_svm_model():
-            # Di sini Anda harus mengganti 'nama_model.pkl' dengan nama file yang sesuai dengan model SVM Anda
-            model = joblib.load('modelbagging.pkl')
-            return model
 
         # Input fields
         Usia = st.number_input("Umur", min_value=0, max_value=150, step=1)
