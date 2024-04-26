c8da63ff345c0b03f07abda94a3bf3370751f873
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-04-26 11:15:19 +0700

    Update tugashipertensi.py

--- tugashipertensi.py
+++ tugashipertensi.py
@@ -258,10 +258,10 @@ def main():
     
             #st.write("Nama Kolom Sebelum Normalisasi:", X_test.columns)
             # Menampilkan hasil kolom "Jenis Kelamin" setelah normalisasi
-            st.write("Hasil Kolom Jenis Kelamin sebelum Normalisasi:", X_test['Jenis Kelamin'])
+            #st.write("Hasil Kolom Jenis Kelamin sebelum Normalisasi:", X_test['Jenis Kelamin'])
             # Menampilkan semua hasil kolom setelah normalisasi
-            st.write("Hasil Kolom Sebelum Normalisasi:")
-            st.write(X_test)
+            #st.write("Hasil Kolom Sebelum Normalisasi:")
+            #st.write(X_test)
             #st.write("Nama Kolom Sebelum Normalisasi:", X_test.columns)
     
             # Normalize the data
