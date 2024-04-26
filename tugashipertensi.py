e2665bb91a3ffe3025456cc5150d635f81a840a4
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-04-26 10:53:25 +0700

    Update tugashipertensi.py

--- tugashipertensi.py
+++ tugashipertensi.py
@@ -249,7 +249,7 @@ def main():
                 'Jenis Kelamin': [gender_binary]
             }
                 
-            X_test.append(pd.DataFrame(data_input), inplace=True)
+            X_test = X_test.append(data_input)
     
             st.write("Nama Kolom Sebelum Normalisasi:", X_test.columns)
             # Menampilkan hasil kolom "Jenis Kelamin" setelah normalisasi
